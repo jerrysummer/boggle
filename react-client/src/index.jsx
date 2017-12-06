18158d51034823785710dd69b8f34eafd51544ac
jerrysummer <reason2be@gmail.com>
2017-12-06 02:55:23 -0800

    Die selection passing back to index

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -17,6 +17,7 @@ class App extends React.Component {
       currentSelection: '',
       selectedWords: []
     }
+    this.handleDieSelect = this.handleDieSelect.bind(this)
   }
 
   componentDidMount() {
