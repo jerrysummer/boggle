17941e632a372b94b177068a55f243032bbf1f3e
jerrysummer <reason2be@gmail.com>
2017-12-10 03:54:19 -0800

    Clean up server

--- server/index.js
+++ server/index.js
@@ -1,24 +1,9 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const port = process.env.PORT || 3000;
-
-const items = require('../database-mongo');
-
 const app = express();
 
 app.use(express.static(`${__dirname}/../react-client/dist`));
-
-
-app.get('/items', (req, res) => {
-  items.selectAll((err, data) => {
-    if (err) {
-      res.sendStatus(500);
-    } else {
-      res.json(data);
-    }
-  });
-});
 
 app.listen(port, () => {
   console.log('listening on port 3000!');
