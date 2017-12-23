d4aeaab4d39f4a75ed91cab3b43b613c2e1e2fc4
jerrysummer <reason2be@gmail.com>
2017-12-22 23:46:48 -0800

    Dice made into 3d

--- README.md
+++ README.md
@@ -7,3 +7,20 @@ https://boggle-game-jerry.herokuapp.com/
 ![boggle demo](./boggleDemo.gif?raw=true "Demo")
 
 
+## Requirements
+
+- Node 6.9.x
+- Jest 21.2.x
+
+## Development
+
+### Installing System Dependencies
+
+
+## Running the App
+
+To run webpack build: `npm run build`
+
+To run server: `npm run start`
+
+To run tests: `npm run test`
--- react-client/src/components/Die.jsx
+++ react-client/src/components/Die.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 const Die = ({ die, pos, handleDieSelect }) => (
-    <div
-      className={die.selected ? 'selected die' : 'die'}
-      onClick={() => handleDieSelect(die.val, pos)}
-    >
+  <div
+    className={die.selected ? 'selected die' : 'die'}
+    onClick={() => handleDieSelect(die.val, pos)}
+  >
+    <div className="die-circle">
       {die.val}
     </div>
-  );
+  </div>
+);
 
 export default Die;
